246145a6345731f35cfdb69bfb052a6c80279c7e
Leda <ledathemis@protonmail.com>
2022-04-12 20:39:41 +0300

    add additional calculator.add() tests

--- calculator.test.js
+++ calculator.test.js
@@ -2,14 +2,17 @@ import calculator from './calculator';
 
 test('Add: a number and zero', () => {
   expect(calculator.add(1, 0)).toBe(1);
+  expect(calculator.add(0, 1)).toBe(1);
 });
 
 test('Add: two numbers', () => {
   expect(calculator.add(2, 3)).toBe(5);
+  expect(calculator.add(3, 2)).toBe(5);
 });
 
 test('Add: positive and negative numbers', () => {
   expect(calculator.add(2, -3)).toBe(-1);
+  expect(calculator.add(-3, 2)).toBe(-1);
 });
 
 test('Subtract: a number and zero', () => {
