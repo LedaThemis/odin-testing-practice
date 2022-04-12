ba12e055e3659fd9ed9773a9e51269d4591f3a68
Leda <ledathemis@protonmail.com>
2022-04-12 20:38:53 +0300

    add calculator.subtract() tests

--- calculator.test.js
+++ calculator.test.js
@@ -11,3 +11,23 @@ test('Add: two numbers', () => {
 test('Add: positive and negative numbers', () => {
   expect(calculator.add(2, -3)).toBe(-1);
 });
+
+test('Subtract: a number and zero', () => {
+  expect(calculator.subtract(0, 2)).toBe(-2);
+  expect(calculator.subtract(2, 0)).toBe(2);
+});
+
+test('Subtract: two positive numbers', () => {
+  expect(calculator.subtract(6, 3)).toBe(3);
+  expect(calculator.subtract(3, 6)).toBe(-3);
+});
+
+test('Subtract: two negative numbers', () => {
+  expect(calculator.subtract(-6, -3)).toBe(-3);
+  expect(calculator.subtract(-3, -6)).toBe(3);
+});
+
+test('Subtract: positive and negative numbers', () => {
+  expect(calculator.subtract(6, -3)).toBe(9);
+  expect(calculator.subtract(-3, 6)).toBe(-9);
+});
