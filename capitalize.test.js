423ef92819781ae5bf39d98c69e8441ced42ea60
Leda <ledathemis@protonmail.com>
2022-04-12 20:23:56 +0300

    add missing expect()

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,21 +1,21 @@
 import capitalize from './capitalize';
 
 test('All lowercase string', () => {
-  capitalize('sample').toBe('Sample');
+  expect(capitalize('sample')).toBe('Sample');
 });
 
 test('All uppercase string', () => {
-  capitalize('SAMPLE').toBe('SAMPLE');
+  expect(capitalize('SAMPLE')).toBe('SAMPLE');
 });
 
 test('Only first letter lowercase', () => {
-  capitalize('sAMPLE').toBe('SAMPLE');
+  expect(capitalize('sAMPLE')).toBe('SAMPLE');
 });
 
 test('Only first letter uppercase', () => {
-  capitalize('Sample').toBe('Sample');
+  expect(capitalize('Sample')).toBe('Sample');
 });
 
 test('Mix of upper/lower case', () => {
-  capitalize('SaMpLe').toBe('SaMpLe');
+  expect(capitalize('SaMpLe')).toBe('SaMpLe');
 });
